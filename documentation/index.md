622ec108330022709ca869b1422e9923390d9b6f
coop-blake <96803255+coop-blake@users.noreply.github.com>
2024-08-29 14:35:56 -0700

    removed project from page repository

--- documentation/index.md
+++
@@ -1,7 +0,0 @@
-# DSN2Sheets
-
-## Values
-
-### range
-
-This is the range that is sent as a both a clear range and the input range for the data received
--- src/utility.rs
+++
@@ -1,9 +0,0 @@
-//function that prints the length of each string in  a vector of vectors of strings
-pub fn print_lengths(data: &Vec<Vec<String>>) -> () {
-    for row in data {
-        for item in row {
-            println!("{} ", item.len());
-        }
-        println!("{} ", row.len());
-    }
-}
